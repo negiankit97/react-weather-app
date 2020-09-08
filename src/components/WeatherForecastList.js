b2b8d9c7719a3bc863d703e89297f6f8def2db34
Ankit Negi <ankit.negi@calsoftinc.com>
2020-09-08 19:14:02 +0530

    Implemented changes to styles & added sidebar nav.

--- src/components/WeatherForecastList.js
+++ src/components/WeatherForecastList.js
@@ -8,23 +8,24 @@ import Loader from './Loader';
 const WeatherForecastList = (props) => {
     return (
         <ul className="weather__report__list">
-            {props.isLoading && <Loader/>}
-            {props.isError  && <div className="error-alert">No result found.</div>}
+            {props.isLoading && <Loader />}
+            {props.isError && <div className="error-alert">No result found.</div>}
             {props.forecastList.length !== 0 && props.forecastList.list.map((report, index) => {
                 return (
-                    <LazyLoad key={index} height={200} offset={100} throttle={2} placeholder={<Loader/>}>
-                        <WeatherCard 
-                        key={index}
-                        desc={report.weather[0].main}
-                        humidity={report.main.humidity}
-                        sunrise={props.forecastList.city.sunrise}
-                        sunset={props.forecastList.city.sunset}
-                        date={report.dt_txt}
-                        feels_like={report.main.feels_like}
-                        mintemp={report.main.temp_min}
-                        maxtemp={report.main.temp_max}/>
-                    </LazyLoad>)}
-                )
+                    <LazyLoad key={index} height={200} offset={100} throttle={2} placeholder={<Loader />}>
+                        <WeatherCard
+                            key={index}
+                            desc={report.weather[0].main}
+                            humidity={report.main.humidity}
+                            sunrise={props.forecastList.city.sunrise}
+                            sunset={props.forecastList.city.sunset}
+                            date={report.dt_txt}
+                            feels_like={report.main.feels_like}
+                            mintemp={report.main.temp_min}
+                            maxtemp={report.main.temp_max} />
+                    </LazyLoad>)
+            }
+            )
             }
         </ul>
     );
--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import WeatherForecastList from './WeatherForecastList';
 import SearchWeatherForecast from './SearchWeatherForecast';
 import '../styles/report.scss';
 import { Link } from 'react-router-dom';
 
 const WeatherReport = (props) => {
+    const menuRef= useRef(null);
+    const listRef= useRef(null);
+    function handleClick() {
+        listRef.current.classList.toggle('open');
+        menuRef.current.classList.toggle('open');
+    }
+
     return (
         <div className="weather__report">
             <header className="weather__report__header">
                 <h1 className="weather__report__title">Weather Forecast</h1>
                 <nav className="weather__report__nav">
-                    <ul className="weather__report__nav__list">
+                    <span ref={menuRef} className="weather__report__nav__menu" onClick={() => handleClick()}></span>
+                    <ul ref={listRef} className="weather__report__nav__list">
                         <li className="weather__report__nav__list__item"><Link to={{ pathname: '/weather' }} state={{ from: props.location }}>Current Weather Data</Link></li>
                         <li className="weather__report__nav__list__item"><Link to={{ pathname: '/4day' }} state={{ from: props.location }}>4 Day Forecast</Link></li>
                         <li className="weather__report__nav__list__item"><Link to={{ pathname: '/16day' }} state={{ from: props.location }}>16 Day Forecast</Link></li>
